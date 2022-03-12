41be6843c3659cd895334d9cd300a601269e552d
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-03-12 18:53:07 +0300

    Initial Commit

---
+++ ChartsUI/ChartsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsUIApp.swift
+//  ChartsUI
+//
+//  Created by Danil Peregorodiev on 12.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChartsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
